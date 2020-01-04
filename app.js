b6b9f8dcd7c3fc4b77c0be39877663ea0cb3d6ef
Mike Rivera <michaelstephenrivera@gmail.com>
2020-01-03 17:52:37 -0800

    Created logic for Deleting a Book from UI

--- app.js
+++ app.js
@@ -48,6 +48,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -56,7 +63,7 @@ UI.prototype.clearFields = function () {
 }
 
 
-// Event Listeners
+// Event Listener for Add Book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -86,6 +93,21 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
 
 
 
+
+  e.preventDefault();
+});
+
+// Event Listener for Delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete Book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
